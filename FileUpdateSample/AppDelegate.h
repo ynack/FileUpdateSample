90c60c204bc577cadaa169e654a8e09df9293360
Yosuke Nakayama <nakayama.yosuke@gmail.com>
2019-05-24 12:53:02 +0900

    Initial Commit

---
+++ FileUpdateSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FileUpdateSample
+//
+//  Created by yosuke on 2019/05/24.
+//  Copyright © 2019 yosuke.nakayama. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ FileUpdateSample/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  FileUpdateSample
+//
+//  Created by yosuke on 2019/05/24.
+//  Copyright © 2019 yosuke.nakayama. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
